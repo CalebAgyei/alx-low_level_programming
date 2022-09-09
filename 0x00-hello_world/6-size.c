5f0b2e4e034cddac3cbe9c0fa1854caa55e6bb04
CalebAgyei <calebnagyei@gmail.com>
2022-09-08 22:51:34 -0700

    Taks 6 - sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,8 @@ int main(void)
 	printf("Size of a char is: %c byte(s)\n", (char)sizeof(c));
 	printf("Size of an int is: %i byte(s)\n", (int)sizeof(i));
 	printf("Size of a long int is: %ld byte(s)\n", (long int)sizeof(ld));
-	printf("Size of a long long int is: %lld byte(s)\n", (long long int)sizeof(lld));
+	printf("Size of a long long int is: %lld byte(s)\n", (long long int)
+			sizeof(lld));
 	printf("Size of a float is: %f byte(s)\n", (float)sizeof(f));
 	return (0);
 }
