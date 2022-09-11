5c43e2f0a4cbdf948bc735f0ba42e936379731cf
CalebAgyei <calebnagyei@gmail.com>
2022-09-10 21:16:58 -0700

    Task 8 - print to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Prints a text to standard error
- * 
  * Return: Returns error value of 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-			);
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
