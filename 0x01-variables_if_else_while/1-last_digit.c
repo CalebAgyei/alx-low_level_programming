c90889369c652acef003f9133cf011a23885b84b
CalebAgyei <calebnagyei@gmail.com>
2022-09-11 10:15:05 -0700

    Task 1 - compare last digit of random number with other metrics

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,11 +8,10 @@
 int main(void)
 {
 	int n;
+	int num;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int num;
-
 	num = n % 10;
 	if (num > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, num);
