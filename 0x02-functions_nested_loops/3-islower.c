c336cab99cffa884b56ca7e106a91662a3c57055
CalebAgyei <calebnagyei@gmail.com>
2022-09-13 20:18:48 -0700

    Task 3 - checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,21 +7,5 @@
  */
 int _islower(int c)
 {
-	char ch = 'a';
-	char r;
-
-	while (ch <= 'z')
-	{
-		if (c == ch)
-		{
-			_putchar('1');
-		}
-		ch++;
-		r = ch;
-	}
-	if (c != r)
-	{
-		_putchar('0');
-	}
-	return (0);
+	return (c >= 'a' && c <= 'z');
 }
