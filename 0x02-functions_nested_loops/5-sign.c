993ffd381ef0df968cd868da429c4503a9406ac0
CalebAgyei <calebnagyei@gmail.com>
2022-09-13 20:59:40 -0700

    Task 5 - prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @c: any value of type int
+ * Return: 0
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+	}
+	else if (c < 0)
+	{
+		_putchar('-');
+	}
+	return (0);
+}
