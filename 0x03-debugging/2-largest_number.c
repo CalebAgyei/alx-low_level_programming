ff6e23140a5e25fc610b935ed106608af417b33a
CalebAgyei <calebnagyei@gmail.com>
2022-09-15 21:23:06 -0700

    Task 2 - prints largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,38 +11,38 @@
 
 int largest_number(int x, int y, int z)
 {
-	/* int largest; */ 
+	int largest;
 
 	if ((x > y) && (y > z))
 	{
-		printf("%d\n", x);
+		largest = x;
 	}
 	else if ((y > x) && (x > z))
 	{
-		printf("%d\n", y);
+		largest = y;
 	}
 	else if ((z > x) && (x > y))
 	{
-		printf("%d\n", z);
+		largest = z;
 	}
 	/* additional code */
 	else if ((x == y) && (x == z))
 	{
-		printf("%d\n", x);
+		largest = x;
 	}
 	else if ((x == y) && (x > z))
 	{
-		printf("%d\n", x);
+		largest = x;
 	}
 	else if ((x == z) && (x > y))
 	{
-		printf("%d\n", x);
+		largest = x;
 	}
 	else if ((y == z) && (y > x))
 	{
-		printf("%d\n", y);
+		largest = y;
 	}
 
 
-	return (0);
+	return (largest);
 }
