5a5a99df766dc59a21d000a80ffbf9f045495cb0
CalebAgyei <calebnagyei@gmail.com>
2022-09-16 10:42:14 -0700

    Task 6 - print straight line

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 - 14
+ * Return:void
+ */
+
+void more_numbers(void)
+{
+	int n, m;
+
+	n = 1;
+
+	while (n <= 10)
+	{
+		for (m
+	}
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
