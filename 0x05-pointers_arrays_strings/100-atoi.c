028491a5ba7c263bd81c230e2504c6b7ea19a7d7
CalebAgyei <calebnagyei@gmail.com>
2022-09-20 17:12:57 -0700

    Advance tasks

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,29 @@
 #include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer to string to be converted
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int h;
+	int i;
+	int len = 0;
+	int p;
+
+
+	for (h = 0; s[h] != '\0'; h++)
+	{
+		len++;
+	}
+
+	if (len != 1)
+	{
+		for (i = 0; s[i] >= '0' && s[i] <= '9'; i++)
+		{
+			p = (int)(s[i]);
+		}
+	}
+	return (0);
+}
