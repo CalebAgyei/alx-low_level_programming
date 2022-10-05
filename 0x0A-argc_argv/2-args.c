e4ba454ca5eafa45165577baa67e15769ed25a50
CalebAgyei <calebnagyei@gmail.com>
2022-10-05 16:51:37 -0700

    Task 2 - Prints all arguments received

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * main - Creates a program that prints all arguments it receives 
+ * main - Creates a program that prints all arguments it receives
  * @argc: Count of arguments.
  * @argv: Array of strings.
  *
- * Description: All arguments should be printed, including the first one 
+ * Description: All arguments should be printed, including the first one
  * Only print one argument per line, ending with a new line
  * Return: 0 (Always success).
  */
