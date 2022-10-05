4496d170a7d7a4be247e93e074092c1b3b521828
CalebAgyei <calebnagyei@gmail.com>
2022-10-05 16:42:51 -0700

    Task 1 - Print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Creates a program that prints the number of arguments
+ * passed into it, followed by a new line. 
+ * @argc: Count of arguments.
+ * @argv: Array of strings.
+ *
+ * Return: 0 (Always success).
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
