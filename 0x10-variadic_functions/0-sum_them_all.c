d4dfc51f5098c54f52b11f0442f8f61220e84385
CalebAgyei <calebnagyei@gmail.com>
2022-10-13 23:19:38 -0700

    Task 1 - sum all parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,18 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	int i;
-	int sum = 0;
+	unsigned int i, sum;
 
 	if (n == 0)
 		return (0);
 
-	va_start(ap, n);
-	for (i = n; i > 0; i = va_arg(ap, const unsigned int))
-		sum += i;
+	va_start(ap, n); /* Initialize the argument list. */
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, const unsigned int); /* Get the next
+		argument value. */
+
+	va_end(ap); /* Clean up. */
 	return (sum);
 }
