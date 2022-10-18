9b3431cfc7e6637143df1290b9440fb72a2a207b
CalebAgyei <calebnagyei@gmail.com>
2022-10-18 00:59:46 -0700

    Task 3 - Print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - Print anything.
+ * @format: List of types or arguments passed to the function
+ * Description:
+ * c: char
+ * i: integer
+ * f: float
+ * s: char * (if the string is NULL, print (nil) instead
+ * any other char should be ignored
+ * You are not allowed to use for, goto, ternary operator, else, do ... while
+ * You can use a maximum of
+ * 2 while loops
+ * 2 if
+ * You can declare a maximum of 9 variables
+ * You are allowed to use printf
+ * Print a new line at the end of your function
+ *
+ * Return: Void.
+ */
+void print_all(const char * const format, ...)
+{
+	int i;
+	char *str, *sep;
+	va_list ap;
+
+	va_start(ap, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(ap, int));
+					break;
+				case 'i':
+					printf("%s%i", sep, va_arg(ap, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(ap, double));
+					break;
+				case 's':
+					str = va_arg(ap, char *);
+					if (!str)
+						str = "(nil)";
+					else
+						printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
